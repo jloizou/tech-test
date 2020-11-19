e9c649c60cd1950faee4bdb6f28a66ba1a50b7d4
Andrew Evans <andrew@aevans.me>
2020-11-19 15:59:02 +0000

    Update Weather.jsx

--- taskfolder/src/components/Weather/Weather.jsx
+++ taskfolder/src/components/Weather/Weather.jsx
@@ -10,29 +10,23 @@ const Weather = () => {
         let cityArr = [{city : "London", ftn: setWeatherLondon},
             {city : "Bristol", ftn: setWeatherBristol},
             {city : "Rome", ftn: setWeatherRome}]
-        cityArr.forEach((obj) => { 
-            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${obj.city}&appid=0f33f5c78acf44e7d38b5f6706f6f59d`)
-            .then(res => res.json())
-            .then(res => { 
-                obj.ftn(res)
-                
-            })
-        })
         
+        cityArr.forEach((city) => { 
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city.city}&appid=0f33f5c78acf44e7d38b5f6706f6f59d`)
+            .then(res => res.json())
+            .then(res => city.ftn(res))
+        })
     }, []);
-
-
 
     return (
         <div>
             <WeatherCards 
-            weatherLondon={weatherLondon}
-            weatherBristol={weatherBristol}
-            weatherRome={weatherRome}
+                weatherLondon={weatherLondon}
+                weatherBristol={weatherBristol}
+                weatherRome={weatherRome}
             />
         </div>
     )
 }
 
-
-export default Weather
+export default Weather;
